190abb8a2c643ef0b96d1654825cb39ea6143e56
JoelKanyi <joelkanyi98@gmail.com>
2024-04-29 12:10:01 +0300

    PREFER_SETTINGS on settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
