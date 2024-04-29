9ee71a7e51c7a3fd1d326dd092f61f4eaddb0e89
JoelKanyi <joelkanyi98@gmail.com>
2024-04-29 12:48:05 +0300

    FAIL_ON_PROJECT_REPOS on settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,19 +2,13 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
