529b119f91b1ff8653a3440e3e6883fe7a719fe7
JoelKanyi <joelkanyi98@gmail.com>
2024-06-04 06:11:27 +0300

    add desktop launcher icons

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
 /*
  * Copyright 2023 Joel Kanyi.
  *
@@ -24,18 +26,46 @@ dependencies {
     implementation(compose.desktop.currentOs)
 }
 
+group = "com.joelkanyi"
+version = "1.0.0"
+
 compose.desktop {
     application {
         mainClass = "DesktopAppKt"
         nativeDistributions {
-            targetFormats(
-                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg,
-                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi,
-                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb,
-                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Rpm
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+
+            packageName = "FocusBloom"
+            packageVersion = project.version as String
+            description = "An  app that helps users enhance their productivity and time management skills through focused work intervals and short breaks."
+            copyright = "© 2023 Joel Kanyi"
+            vendor = "Joel Kanyi"
+
+            // .gradlew suggestRuntimeModules
+            modules(
+                "java.instrument",
+                "java.management",
+                "java.prefs",
+                "java.sql",
+                "jdk.unsupported"
             )
-            packageName = "focusbloom"
-            packageName = "1.0.0"
+
+            val iconsRoot = project.file("src/main/resources/drawables")
+
+            linux {
+                iconFile.set(iconsRoot.resolve("launcher_icons/linuxos.png"))
+            }
+
+            windows {
+                iconFile.set(iconsRoot.resolve("launcher_icons/windowsos.ico"))
+                upgradeUuid = "31575EDF-D0D5-4CEF-A4D2-7562083D6D88"
+                menuGroup = packageName
+                perUserInstall = true
+            }
+
+            macOS {
+                iconFile.set(iconsRoot.resolve("launcher_icons/macos.icns"))
+            }
         }
     }
 }
